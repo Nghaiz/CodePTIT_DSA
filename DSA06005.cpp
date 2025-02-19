45192a0e7783595cbffc534981db266fc4e245ac
Nghaiz <Nghiaa0.NTT@gmail.com>
2025-02-19 17:14:45 +0700

    add file

---
+++ DSA06005.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define MOD 1000000007
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--){
+        int n, m, x;
+        cin >> n >> m;
+        set<int> hop, giao;
+
+        for (int i = 0; i < n; i++){
+            cin >> x;
+            hop.insert(x);
+        }
+        for (int i = 0; i < m; i++){
+            cin >> x;
+            if (hop.count(x)) giao.insert(x);
+
+            hop.insert(x);
+        }
+
+        for (int x : hop) cout << x << " ";
+        cout << "\n";
+        for (int x : giao) cout << x << " ";
+        cout << "\n";
+    }
+    return 0;
+}
